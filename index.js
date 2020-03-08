5dfb1526cf9b04684f776c880080e71f930cbd95
Ariunbold <ariun1118@gmail.com>
2020-03-08 19:48:31 -0400

    Installed all the dependecies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express")
 
 const server = express()
 const port = 4000
@@ -6,7 +6,11 @@ const port = 4000
 server.use(express.json())
 
 server.get("/", (req, res) => {
-    res.send(`<h2>Sprint Challenge for DB</h2>`)
+    res.send(`<h2>Sprint Challenge DB</h2>`)
+})
+
+server.use(port, () => {
+    console.log(`Listening on ${port}...`)
 })
 
 server.use((req, res) => {
@@ -15,11 +19,8 @@ server.use((req, res) => {
     })
 })
 
-server.use((err, req, res, next) => {
+server.use((err,req, res, next) => {
     res.status(500).json({
         message: "Error retrieving the Data"
     })
-})
-server.listen(port, () => {
-  console.log( `Server is running on ${port}...`) 
 })
